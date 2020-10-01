e628a726f72af6195e531107619524f8f6c51994
avil13 <13.avil@gmail.com>
2020-10-01 15:28:24 +0300

    feat: the division into packages

--- packages/act-master-devtools/scripts/devtools.js
+++
@@ -1,6 +0,0 @@
-// Can use
-// chrome.devtools.*
-// chrome.extension.*
-
-// Create a tab in the devtools area
-chrome.devtools.panels.create("DemoPanel", "toast.png", "panel.html", function(panel) {});
---
+++ packages/act-master-devtools/src/scripts/devtools.js
@@ -0,0 +1,6 @@
+// Can use
+// chrome.devtools.*
+// chrome.extension.*
+
+// Create a tab in the devtools area
+chrome.devtools.panels.create('ACT-Master', 'toast.png', 'panel.html', function (panel) { });
