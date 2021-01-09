069ab8ad17495d1c8186a1dbe5d87f9c1be8842b
avil13 <13.avil@gmail.com>
2021-01-09 05:12:05 +0000

    Deploying to gh-pages from  @ 4067816b7fc899fc64b834bcc95d7ee84afb7849 🚀

--- assets/action_02-add-action.md.88def1cf.lean.js
+++
@@ -1,1 +0,0 @@
-import{l as n,f as a,G as s}from"./framework.8207f0d3.js";const t='{"title":"Add Actions to Vue-act-master","description":"","frontmatter":{},"relativePath":"action/02-add-action.md","lastUpdated":1610168479468}',p={},o=s('',8);p.render=function(s,t,p,c,e,u){return n(),a("div",null,[o])};export default p;export{t as __pageData};
