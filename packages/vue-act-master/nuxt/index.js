c8525524d9526d211e0b265ea22f70948441de6b
avil13 <13.avil@gmail.com>
2021-05-07 19:24:01 +0300

    feat(vue-act-master): nuxt config options

--- packages/vue-act-master/nuxt/index.js
+++ packages/vue-act-master/nuxt/index.js
@@ -2,11 +2,13 @@
 const { resolve } = require('path');
 
 module.exports = function nuxtVueActMaster(moduleOptions) {
+  const options = Object.assign({}, (this.options.actMaster || {}), moduleOptions);
+
   // Register plugin
   this.addPlugin({
     src: resolve(__dirname, 'plugin.js'),
     fileName: 'vue-act-master.js',
-    options: moduleOptions,
+    options,
     ssr: true,
   });
 };
