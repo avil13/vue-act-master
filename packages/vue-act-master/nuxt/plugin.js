41a2444bdd01326a4e77b1dad1bcf1d11be23493
avil13 <13.avil@gmail.com>
2021-05-08 20:46:28 +0300

    feat(vue-act-master): nuxt fix module 2

--- packages/vue-act-master/nuxt/plugin.js
+++ packages/vue-act-master/nuxt/plugin.js
@@ -4,6 +4,6 @@ import { VueActMaster } from 'vue-act-master';
 Vue.use(VueActMaster, <%= JSON.stringify(options, null, 2) %>);
 
 export default (ctx, inject) => {
-  ctx.$act = Vue.$act;
+  ctx.$act = Vue.act;
   inject('act', Vue.$act);
 }
