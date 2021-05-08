8d86d1866f08d2338132af70ca0ef2f1bb483526
avil13 <13.avil@gmail.com>
2021-05-08 23:18:25 +0300

    feat(vue-act-master): nuxt fix module 4

--- packages/vue-act-master/nuxt/plugin.js
+++ packages/vue-act-master/nuxt/plugin.js
@@ -1,7 +1,25 @@
 import Vue from 'vue';
 import { VueActMaster } from 'vue-act-master';
 
-Vue.use(VueActMaster, <%= JSON.stringify(options, null, 2) %>);
+import actions from '<%= options.actions %>';
+
+<% if (options.di) { %>
+import di from '<%= options.di %>';
+<% } else { %>
+  const di = {};
+<% } %>
+
+const actionsList = actions && actions.actions || actions;
+const diItems = di && di.di || di;
+
+
+const params = {
+  ...<%= JSON.stringify(options, null, 2) %>,
+  actions: actionsList,
+  di: diItems,
+};
+
+Vue.use(VueActMaster, params);
 
 export default (ctx, inject) => {
   ctx.$act = Vue.act;
