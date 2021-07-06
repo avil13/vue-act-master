a6cd619be3b9adb40a20efc018b94c9c5e0ef4d4
avil13 <13.avil@gmail.com>
2021-07-06 18:30:22 +0300

    feat(vue-act-master): nuxt remove default exports

--- packages/vue-act-master/nuxt/plugin.js
+++ packages/vue-act-master/nuxt/plugin.js
@@ -1,22 +1,20 @@
 import Vue from 'vue';
 import { VueActMaster } from 'vue-act-master';
 
-import actionsDefault from '<%= options.actions %>';
 import { actions } from '<%= options.actions %>';
 
 <% if (options.di) { %>
-import di from '<%= options.di %>';
+import { di } from '<%= options.di %>';
 <% } else { %>
 const di = {};
 <% } %>
 
-const actionsList = actions || actionsDefault;
-const diItems = di && di.di || di;
+const diItems = di;
 
 
 const params = {
   ...<%= JSON.stringify(options, null, 2) %>,
-  actions: actionsList,
+  actions: actions,
   di: diItems,
 };
 
