37cec657f561c1fa2f2ef9a54633ea5f2fc8e715
avil13 <13.avil@gmail.com>
2023-05-17 07:22:16 +0000

    Deploying to gh-pages from @ avil13/vue-act-master@ab3f70651e6ce9bf1e5a777bee66e07650ce99d3 🚀

--- assets/advanced_09-function-to-action.md.f042b38c.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as n,c as a,o as s,a as t}from"./app.2a3f081e.js";const _='{"title":"Convert function to action","description":"","frontmatter":{},"relativePath":"advanced/09-function-to-action.md","lastUpdated":1648975299916}',o={},c=t("",5),p=[c];function e(u,i,l,r,k,d){return s(),a("div",null,p)}var m=n(o,[["render",e]]);export{_ as __pageData,m as default};
